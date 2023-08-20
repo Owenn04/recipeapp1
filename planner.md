21c9e3c02155a9ecb7f1620078e3aad5a66eacb8
Owen Sawler <owensawler4@gmail.com>
2023-08-19 23:07:39 -0400

    day 1

---
+++ planner.md
@@ -0,0 +1,6 @@
+#Day 1
+- built home page base layout.
+
+#Day 2
+- some stuff with config
+- look into setting up db or more frontend
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,34 @@
-
+import './home.css'
+import Navbar from './components/navbar'
+{/* <main className="flex min-h-screen flex-col items-center justify-between p-24"></main> */}
 //This is the home page
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <h1>Home</h1>
+    <div>
+      <div className="navbar">
+        <Navbar/>
       </div>
-    </main>
-  )
+      <div className="main-section">
+        <div className="title-bar">
+          {/* Title content goes here */}
+        </div>
+        <div className="secondary-navbar">
+          {/* Secondary navbar content goes here */}
+        </div>
+        <div className="content-wrapper">
+          <div className="left-section">
+            {/* Left section content goes here */}
+            <p>left</p>
+          </div>
+          <div className="right-section">
+            {/* Right section content goes here */}
+            <p>right</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
+    
+
